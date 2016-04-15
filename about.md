5b7c544d37407696de5380053bdebfcd64aeb7fc
victor <patgod85@gmail.com>
2016-04-15 11:11:48 +0300

    Script for a publishing. Amend2

--- about.md
+++ about.md
@@ -4,6 +4,6 @@ title: About
 permalink: /about/
 ---
 
-Look at you
+Look at you. And you will see
 
 ![Всё что я хотел рассказать о войне во Вьетнаме](/assets/images/zabriskiepoint.jpg)
--- git-publish-io.bat
+++ git-publish-io.bat
@@ -4,5 +4,8 @@ git checkout -b master
 git add _site/*
 git commit -m "auto commit by git-publish-io"
 git filter-branch --subdirectory-filter _site/ -f
-git checkout source
+git rm -rf --cached git-publish-io.bat
+git commit -m "auto commit by git-publish-io. Remove bat file from master"
+git checkout -f source
+git push origin --delete master
 git push --all origin
